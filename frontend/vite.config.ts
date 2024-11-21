5988953c0d91c74079c6dc2731c66b9b6da73022
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-21 09:34:06 -0300

    feat: adiciona configuracoes de server no vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      script: {
+        propsDestructure: true,
+        defineModel: true,
+      }
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: 8181,
+    proxy: {
+      '/api': {
+        target: 'https://api.exemplo.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 })
