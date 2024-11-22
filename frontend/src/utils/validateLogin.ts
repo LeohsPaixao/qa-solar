aec329ca64a75a96835db8ec41eb84eab9e3027c
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-22 17:56:16 -0300

    feat: cria funcoes auxiliares para validar o login

---
+++ frontend/src/utils/validateLogin.ts
@@ -0,0 +1,20 @@
+export function validateEmail(value: string): string {
+  if (!value) {
+    return 'O email é obrigatório.';
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(value)) {
+    return 'Por favor, insira um email válido.';
+  }
+  return '';
+};
+
+export function validatePassword(value: string): string {
+  if (!value) {
+    return 'A senha é obrigatória.';
+  }
+  if (value.length < 6) {
+    return 'A senha deve ter pelo menos 6 caracteres.';
+  }
+  return '';
+};
