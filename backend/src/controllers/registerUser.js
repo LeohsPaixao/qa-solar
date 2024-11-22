d30aa877bc4aedd59419e44c89d4e69c0872190c
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-22 17:43:42 -0300

    feat: envia a senha criptografada no banco

--- backend/src/controllers/registerUser.js
+++ backend/src/controllers/registerUser.js
@@ -1,16 +1,19 @@
 const pool = require("../config/database");
+const bcrypt = require("bcryptjs");
 
 module.exports.registerUser = async (req, res) => {
   const { fullName, socialName, document, docType, phone, email, password } = req.body;
 
   try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     await pool.query(
       `INSERT INTO users (full_name, social_name, document, doc_type, phone, email, password) 
        VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-      [fullName, socialName, document, docType, phone, email, password]
+      [fullName, socialName, document, docType, phone, email, hashedPassword]
     );
 
-    res.status(201).json({ message: "Usuário cadastrado no QA Solar com sucesso!" });
+    res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
   } catch (error) {
     res.status(500).json({ message: "Erro ao tentar cadastrar o usuário." });
   }
