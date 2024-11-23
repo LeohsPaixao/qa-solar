0c7df6e69682274eb28746aa710bc1c672852b56
Leonardo Paixão <42840902+LeohsPaixao@users.noreply.github.com>
2024-11-23 02:51:19 -0300

    Feat: Ajusta a configuração do playwright (#26)
    * feat: instala os pacotes necessarios do playwright
    
    * feat: ajusta a configuracao inicial do framework playwright
    
    * feat: configura a estrutura de pasta do projeto playwright
    
    * feat: ajusta alguns comandos no git actions do projeto
    
    * feat: ajusta padroes de outros projetos para similares a este
    
    * feat: adiciona o changeset
    
    * feat: ajusta a configuracao do playwright

--- test/playwright/playwright.config.ts
+++ test/playwright/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://127.0.0.1:8181',
+    baseURL: 'http://localhost:8181',
     trace: 'on-first-retry',
   },
   projects: [
@@ -25,4 +25,9 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
   ],
+  webServer: {
+    command: 'yarn api',
+    url: 'https://localhost:3001',
+    reuseExistingServer: true,
+  }
 });
