10c4fd3aa7900db8443e72eb57c5a48ccf9f8ac9
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-24 20:40:45 -0300

    feat: teste #18 CICD

--- test/playwright/tests/specs/login.spec.ts
+++ test/playwright/tests/specs/login.spec.ts
@@ -11,8 +11,55 @@ test.describe('Tela de Login', () => {
     await expect(page.locator('[data-testid="logo"]')).toBeVisible();
     await expect(page.locator('[data-testid="input-email"]')).toBeVisible();
     await expect(page.locator('[data-testid="input-password"]')).toBeVisible();
-    const loginButton = page.locator('[data-testid="btn-login"]');
-    await expect(loginButton).toBeVisible();
-    await expect(loginButton).toBeDisabled();
+    await expect(page.locator('[data-testid="btn-login"]')).toBeVisible();
+    await expect(page.locator('[data-testid="btn-login"]')).toBeDisabled();
+  });
+
+  test('Não deveria ser possível fazer login com credenciais inválidas', async ({ page }) => {
+    await expect(page.locator('[data-testid="btn-login"]')).toBeDisabled();
+    await page.locator('[data-testid="input-email"]').fill('email@example.com');
+    await page.locator('[data-testid="input-password"]').fill('password@example.com');
+    await expect(page.locator('[data-testid="btn-login"]')).toBeEnabled();
+    await page.locator('[data-testid="btn-login"]').click();
+    const toastContent = page.locator('[data-testid="toast-content"]');
+    await expect(toastContent).toBeVisible();
+    await expect(toastContent).toHaveText('Usuário não encontrado.');
+  });
+
+  test('Não deveria ser possível fazer login com a senha inválida', async ({ page }) => {
+    await expect(page.locator('[data-testid="btn-login"]')).toBeDisabled();
+    await page.locator('[data-testid="input-email"]').fill('generic@example.com');
+    await page.locator('[data-testid="input-password"]').fill('password@example.com');
+    await expect(page.locator('[data-testid="btn-login"]')).toBeEnabled();
+    await page.locator('[data-testid="btn-login"]').click();
+    const toastContent = page.locator('[data-testid="toast-content"]');
+    await expect(toastContent).toBeVisible();
+    await expect(toastContent).toHaveText('A senha não confere.');
+  });
+
+  test('Deveria ser possível fazer login com credenciais válidas', async ({ page }) => {
+    await expect(page.locator('[data-testid="btn-login"]')).toBeDisabled();
+    await page.locator('[data-testid="input-email"]').fill('generic@example.com');
+    await page.locator('[data-testid="input-password"]').fill('123456');
+    await expect(page.locator('[data-testid="btn-login"]')).toBeEnabled();
+    await page.locator('[data-testid="btn-login"]').click();
+  });
+
+  test('Deveria ser possível ir para a tela de cadastro', async ({ page, baseURL }) => {
+    await page.locator('[data-testid="link-singup"]').click();
+    await expect(page).toHaveURL(`${baseURL}/signup`);
+    const url = new URL(page.url());
+    expect(url.origin).toBe(baseURL);
+    expect(url.pathname).toBe('/signup');
+    expect(url.protocol).toBe('http:');
+  });
+
+  test.skip('Deveria ser possível ir para a tela de esqueci a senha', async ({ page, baseURL }) => {
+    await page.locator('[data-testid="link-singup"]').click();
+    await expect(page).toHaveURL(`${baseURL}/recover-password`);
+    const url = new URL(page.url());
+    expect(url.origin).toBe(baseURL);
+    expect(url.pathname).toBe('/recover-password');
+    expect(url.protocol).toBe('http:');
   });
 });
