04de1648064f2f618503aa3bbf9c48e2b8cddab6
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-27 08:46:05 -0300

    feat: deixa o codigo mais legivel

--- backend/src/controllers/loginUser.js
+++ backend/src/controllers/loginUser.js
@@ -1,36 +1,36 @@
-import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function loginUser(req, res) {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   try {
     const user = await prisma.user.findUnique({
       where: { email },
-    })
+    });
 
     if (!user) {
-      return res.status(400).json({ message: 'Usuário não encontrado.' })
+      return res.status(400).json({ message: 'Usuário não encontrado.' });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password)
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(400).json({ message: 'A senha não confere.' })
+      return res.status(400).json({ message: 'A senha não confere.' });
     }
 
     const token = jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_SECRET, {
       expiresIn: '1h',
-    })
+    });
 
     res.status(200).json({
       message: 'Login realizado com sucesso!',
       token,
-    })
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Erro ao tentar realizar o login.' })
+    res.status(500).json({ message: 'Erro ao tentar realizar o login.' });
   }
 }
