07375ae498006c6f57d95eeab6c38435596d310c
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-27 09:02:05 -0300

    test(368): adiciona headers na api do axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('user-token');
 const api = axios.create({
   baseURL: 'http://localhost:3001',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 export default api;
