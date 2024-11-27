8c677e2950aa25e309c5bc2c29f3349fce2d7010
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-27 08:47:01 -0300

    feat: cria o controlador para atualizar dados do usuario

---
+++ backend/src/controllers/updateUser.js
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
+export async function updateUser(req, res) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Token não fornecido.' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { fullName, socialName, phone, document } = req.body;
+
+    await prisma.user.update({
+      where: { id: decoded.userId },
+      data: {
+        full_name: fullName,
+        social_name: socialName || null,
+        phone: phone || null,
+        document: document,
+      },
+    });
+
+    res.status(200).json({ message: 'Usuário alterado com sucesso.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro ao atualizar o perfil do usuário.' });
+  }
+}
