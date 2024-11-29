5d2c114e2373dcdcf5c84a90de185032f4885e3c
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-28 22:46:05 -0300

    feat: cria hooks para buscar por email e logout

---
+++ frontend/src/hooks/useFetchEmailUser.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/vue-query';
+import api from '../services/api';
+
+const fetchEmailUser = async (email) => {
+  const response = await api.get('/useremail', {
+    params: { email },
+  });
+
+  return response.data;
+};
+
+export const useFetchEmailUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: fetchEmailUser,
+    onSuccess: () => {
+      queryClient.resetQueries();
+      queryClient.invalidateQueries();
+    },
+  });
+};
