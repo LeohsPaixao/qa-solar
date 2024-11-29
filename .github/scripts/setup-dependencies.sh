a59980e0a65433cfc46c124099460e5ff48b28b7
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-29 15:36:23 -0300

    feat: ajustes finais #1

--- .github/scripts/setup-dependencies.sh
+++ .github/scripts/setup-dependencies.sh
@@ -3,7 +3,7 @@
 # Atualiza os pacotes disponíveis
 sudo apt-get update
 
-# Instala dependências necessárias para o Playwright e bibliotecas extras
+# Instala dependências necessárias para o Playwright e navegadores
 sudo apt-get install -y \
     wget \
     unzip \
@@ -12,16 +12,12 @@ sudo apt-get install -y \
     libgconf-2-4 \
     libwoff1 \
     libopus0 \
-    libwebp6 \
-    libwebpdemux2 \
-    libenchant1c2a \
-    libgudev-1.0-0 \
+    libwebp-dev \
+    libenchant-2-2 \
     libsecret-1-0 \
     libhyphen0 \
     libgles2-mesa \
     libegl1 \
     libfreetype6 \
-    libwoff2-1 \
     fonts-liberation \
-    libx264-155 \
     libflite1
