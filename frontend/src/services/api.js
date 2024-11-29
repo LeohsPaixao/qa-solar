d68ca400db0851da999788f51e554cb97c29eb67
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-28 22:53:25 -0300

    feat: adiciona um interpectacao para validar o token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,24 @@
 import axios from 'axios';
 
 const token = localStorage.getItem('user-token');
+
 const api = axios.create({
   baseURL: 'http://localhost:3001',
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
