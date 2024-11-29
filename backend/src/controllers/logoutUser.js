1484052d9f5761e674fba6e691852dd3cb9861bf
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-29 14:41:09 -0300

    feat: remove os consoles do endpoint

--- backend/src/controllers/logoutUser.js
+++ backend/src/controllers/logoutUser.js
@@ -1,21 +1,13 @@
 export async function logoutUser(req, res) {
   try {
-    const authHeader = req.headers.authorization;
-    console.log('Authorization Header:', authHeader);
+    const token = req.headers.authorization.split(' ')[1];
 
-    if (!authHeader) {
+    if (!token) {
       return res.status(401).json({ message: 'Token não fornecido.' });
     }
 
-    const token = authHeader.split(' ')[1];
-    console.log('Token:', token);
-
-    // Opcional: Salve o token numa blacklist (ou apenas apague do frontend para casos simples)
-    // await prisma.tokenBlacklist.create({ data: { token } });
-
     res.status(200).json({ message: 'Logout realizado com sucesso' });
   } catch (error) {
-    console.error('Erro ao fazer logout:', error);
     res.status(500).json({ message: 'Erro ao processar logout' });
   }
 }
