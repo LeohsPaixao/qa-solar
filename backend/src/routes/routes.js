15eb810a759e9c7f5828346cb7391d25f2dd9851
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-28 22:38:48 -0300

    feat: adiciona novas rodas de endpoint

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import { getEmailUser } from '../controllers/getEmailUser.js';
 import { getUser } from '../controllers/getUser.js';
 import { loginUser } from '../controllers/loginUser.js';
+import { logoutUser } from '../controllers/logoutUser.js';
 import { registerUser } from '../controllers/registerUser.js';
 import { updateUser } from '../controllers/updateUser.js';
 
 const router = Router();
 
 router.post('/register', registerUser);
+router.get('/useremail', getEmailUser);
 router.post('/login', loginUser);
 router.put('/user', updateUser);
-router.get('/user', getUser);
+router.post('/user', getUser);
+router.post('/logout', logoutUser);
 
 export default router;
