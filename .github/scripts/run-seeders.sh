761161464ea01fbffb7a7badc929041862ca91c3
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-30 11:49:11 -0300

    feat: arruma o workflow

--- .github/scripts/run-seeders.sh
+++ .github/scripts/run-seeders.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Navegando para o diretório backend..."
+cd backend || { echo "Falha ao acessar o diretório backend"; exit 1; }
+
 echo "Executando migrações do Prisma..."
 yarn prisma migrate dev
 
@@ -9,5 +12,5 @@ yarn ts-node prisma/seeders/genericUser.js
 echo "Executando seeder: generateUserFake..."
 yarn ts-node prisma/seeders/generateUserFake.js
 
-# Caso tenha outros seeders, adicione-os aqui
+# Caso adicione mais seeders, inclua os comandos aqui
 echo "Seeders executados com sucesso!"
