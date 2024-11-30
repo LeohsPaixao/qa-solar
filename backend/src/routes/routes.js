bed4af21e0c7eb440a83600a27d72941e1f8968f
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-30 00:07:23 -0300

    feat: adiciona novos endpoints

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,18 +1,23 @@
 import { Router } from 'express';
+import { deleteUser } from '../controllers/deleteUser.js';
+import { getAllUsers } from '../controllers/getAllUsers.js';
 import { getEmailUser } from '../controllers/getEmailUser.js';
 import { getUser } from '../controllers/getUser.js';
 import { loginUser } from '../controllers/loginUser.js';
 import { logoutUser } from '../controllers/logoutUser.js';
 import { registerUser } from '../controllers/registerUser.js';
 import { updateUser } from '../controllers/updateUser.js';
+import { authenticate } from '../middleware/auth.js';
 
 const router = Router();
 
 router.post('/register', registerUser);
-router.get('/useremail', getEmailUser);
 router.post('/login', loginUser);
-router.put('/user', updateUser);
 router.post('/user', getUser);
 router.post('/logout', logoutUser);
+router.get('/useremail', getEmailUser);
+router.get('/users', getAllUsers);
+router.put('/user/update', authenticate, updateUser);
+router.delete('/user/delete', authenticate, deleteUser);
 
 export default router;
