00a0f39f9ee23ea8259fe9acf95f83728006cc32
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-30 19:49:15 -0300

    feat: finaliza a nova versao do backend

--- backend/src/controllers/getAllUsers.ts
+++ backend/src/controllers/getAllUsers.ts
@@ -5,31 +5,27 @@ import { formatDateTime } from '../utils/formatDate.js';
 const prisma = new PrismaClient();
 
 export async function getAllUsers(req: Request, res: Response): Promise<void> {
-  try {
-    const users = await prisma.user.findMany({
-      select: {
-        id: true,
-        full_name: true,
-        social_name: true,
-        email: true,
-        phone: true,
-        document: true,
-        created_at: true,
-      },
-    });
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      full_name: true,
+      social_name: true,
+      email: true,
+      phone: true,
+      document: true,
+      created_at: true,
+    },
+  });
 
-    if (users.length === 0) {
-      res.status(404).json({ message: 'Nenhum usuário encontrado.' });
-      return
-    }
-
-    const formattedUsers = users.map((user) => ({
-      ...user,
-      created_at: formatDateTime(user.created_at),
-    }));
-
-    res.status(200).json(formattedUsers);
-  } catch (error) {
-    res.status(500).json({ message: 'Erro ao buscar usuários.' });
+  if (users.length === 0) {
+    res.status(404).json({ message: 'Nenhum usuário encontrado.' });
+    return
   }
+
+  const formattedUsers = users.map((user) => ({
+    ...user,
+    created_at: formatDateTime(user.created_at),
+  }));
+
+  res.status(200).json(formattedUsers);
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,30 @@
-import chalk from 'chalk';
 import dotenv from 'dotenv';
+import winston from 'winston';
 import app from './app.ts';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => `[${timestamp}] ${level}: ${message}`)
+  ),
+  transports: [new winston.transports.Console()],
+});
+
 const startServer = async () => {
   try {
     app.listen(PORT, () => {
-      console.log(chalk.green.bold(`\n🚀 Servidor iniciado com sucesso!`));
-      console.log(chalk.blue(`🌍 URL: http://localhost:${PORT}`));
-      console.log(chalk.yellow(`📅 Iniciado em: ${new Date().toLocaleString()}`));
+      logger.info(`🚀 Servidor iniciado com sucesso!`);
+      logger.info(`🌍 URL: http://localhost:${PORT}`);
+      logger.info(`📅 Iniciado em: ${new Date().toLocaleString()}`);
     });
   } catch (error: any) {
-    console.error(chalk.red.bold(`❌ Erro ao iniciar o servidor:`), error.message);
+    logger.error(`❌ Erro ao iniciar o servidor: ${error.message}`);
     process.exit(1);
   }
 };
