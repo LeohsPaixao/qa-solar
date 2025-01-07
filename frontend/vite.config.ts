0842e536fa0d208ee1d20cd23ced3ec8e9105da0
Leonardo Paixão <42840902+LeohsPaixao@users.noreply.github.com>
2025-01-07 11:20:29 -0300

    FEAT: Instala e configura o plugin de instrumentar o codigo (#87)
    * feat: instala o plugin vite-istanbul
    
    * feat: configura o plugin na config do vite
    
    * feat: adiciona o changeset

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import istanbul from 'vite-plugin-istanbul';
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,12 @@ export default defineConfig({
         propsDestructure: true,
         defineModel: true,
       }
+    }),
+    istanbul({
+      include: 'src/**/*',
+      exclude: ['node_modules', 'test/**/*'],
+      extension: ['.js', '.ts', '.vue'],
+      cypress: true,
     }),
     vueDevTools(),
   ],
