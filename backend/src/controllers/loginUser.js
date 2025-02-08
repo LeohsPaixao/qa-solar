1fb8144f0f9092693cf651629d0b3ea9d2f05405
Leonardo Paixao <leonardohspaixao@gmail.com>
2025-02-07 23:23:45 -0300

    feat: documenta todos os controllers

--- backend/src/controllers/loginUser.js
+++ backend/src/controllers/loginUser.js
@@ -7,6 +7,49 @@ dotenv.config();
 
 const prisma = new PrismaClient();
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Realiza o login do usuário
+ *     description: >
+ *       Efetua o login do usuário e retorna um token JWT que deverá ser utilizado para autenticar as requisições em endpoints protegidos.
+ *       O token deve ser enviado no header Authorization com o prefixo "Bearer".
+ *     tags:
+ *       - Autenticação
+ *     requestBody:
+ *       description: Credenciais do usuário para login
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Email do usuário.
+ *                 example: "generic@example.com"
+ *               password:
+ *                 type: string
+ *                 description: Senha do usuário.
+ *                 example: "123456"
+ *     responses:
+ *       200:
+ *         description: Login efetuado com sucesso e token JWT gerado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: "Bearer <seu_token_jwt>"
+ *       401:
+ *         description: Credenciais inválidas.
+ */
 export async function loginUser(req, res) {
   const { email, password } = req.body;
 
