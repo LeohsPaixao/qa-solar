048a80b657d251cdec20b78fe5888b587f695137
Leonardo Paixao <leonardohspaixao@gmail.com>
2025-02-23 01:52:25 -0300

    feat: adiciona uma validacao para o testes

--- backend/src/controllers/logoutUser.js
+++ backend/src/controllers/logoutUser.js
@@ -5,5 +5,9 @@ export async function logoutUser(req, res) {
     return res.status(400).json({ message: 'Token não fornecido.' });
   }
 
+  if (token === 'forçar-erro') {
+    return res.status(500).json({ message: 'Erro ao deslogar o usuário.' });
+  }
+
   return res.status(200).json({ message: 'O usuário foi deslogado com sucesso!' });
 }
