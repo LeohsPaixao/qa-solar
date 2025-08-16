413153d44b852d08cdaef87324919b11e0fc1135
Leonardo Paixão <42840902+LeohsPaixao@users.noreply.github.com>
2025-08-16 20:36:45 -0300

    FEAT: Remove os arquivos de tutoriais e adiciona a doc do projeto (#176)
    * feat: remove os arquivos de tutoriais e adiciona a doc do projeto
    
    * feat: adiciona o changeset

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import type { ReactNode } from 'react';
 
 import styles from './index.module.css';
 
@@ -16,12 +16,12 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">Monorepo para estudos de desenvolvimento e testes automatizados</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Ver Documentação 📚
           </Link>
         </div>
       </div>
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} - Documentação`}
+      description="Monorepo para estudos de desenvolvimento e testes automatizados com Vue 3, NestJS, Cypress, Playwright, Robot Framework e K6">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
