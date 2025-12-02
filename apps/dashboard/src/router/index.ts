2e3f25195a66040f82bd3d96a4dff75018c28592
Leonardo Paixão <leonardohspaixao@gmail.com>
2025-12-02 18:23:19 -0300

    feat: termina de configurar as rotas e pinioa

--- apps/dashboard/src/router/index.ts
+++ apps/dashboard/src/router/index.ts
@@ -1,26 +1,29 @@
+import FrameworkPage from '@/features/frameworks/FrameworkPage.vue';
+import OverviewPage from '@/features/overview/OverviewPage.vue';
+import TestsListPage from '@/features/tests/TestsListPage.vue';
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Overview',
-    component: () => import('../features/overview/OverviewPage.vue'),
+    component: OverviewPage,
   },
   {
     path: '/frameworks',
     name: 'Frameworks',
-    component: () => import('../features/frameworks/FrameworkPage.vue'),
+    component: FrameworkPage,
   },
   {
     path: '/frameworks/:name',
     name: 'FrameworkDetail',
-    component: () => import('../features/frameworks/FrameworkPage.vue'),
+    component: FrameworkPage,
     props: true,
   },
   {
     path: '/tests',
     name: 'Tests',
-    component: () => import('../features/tests/TestsListPage.vue'),
+    component: TestsListPage,
   },
   {
     path: '/:pathMatch(.*)*',
