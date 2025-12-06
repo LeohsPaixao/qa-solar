11c2f23c4a6bedb065bda09fc946bf93280b9a9b
Leonardo Paixão <leonardohspaixao@gmail.com>
2025-12-06 01:26:28 -0300

    feat: altera de ts-node para tsx

--- .github/scripts/run-seeders.sh
+++ .github/scripts/run-seeders.sh
@@ -8,11 +8,10 @@ cd apps/backend || { echo "Falha ao acessar o diretório backend"; exit 1; }
 echo "Executando migrações do Prisma..."
 yarn prisma migrate dev
 
-echo "Executando seeder: genericUser..."
-yarn ts-node prisma/seeders/genericUser.ts
-
 echo "Executando seeder: generateUserFake..."
-yarn ts-node prisma/seeders/generateUserFake.ts
+yarn tsx prisma/seeders/generateUserFake.ts
 
-# Caso adicione mais seeders, inclua os comandos aqui
+echo "Executando seeder: genericUser..."
+yarn tsx prisma/seeders/genericUser.ts
+
 echo "Seeders executados com sucesso!"
